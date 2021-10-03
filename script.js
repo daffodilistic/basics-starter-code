7a142548cb1a5e36719f709c8c5ba2f919f2baeb
Soh Thiam Hing <daffodilistic@gmail.com>
2021-10-03 21:43:45 +0800

    Move Javascript logic to script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Define button click functionality
+var button = document.querySelector("#submit-button");
+button.addEventListener("click", function () {
+  // Set result to input value
+  var input = document.querySelector("#input-field");
+  var result = main(input.value);
+
+  // Display result in output element
+  var output = document.querySelector("#output-div");
+  output.innerHTML = result;
+
+  // Reset input value
+  input.value = "";
+});
+
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
