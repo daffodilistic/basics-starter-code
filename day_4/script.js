aca3f758ea44be45f8a8c736513ab6576d532ce2
Soh Thiam Hing <daffodilistic@gmail.com>
2021-10-14 18:19:39 +0800

    Finished all exercises for Day 4

--- day_4/script.js
+++ day_4/script.js
@@ -12,20 +12,41 @@ const EXERCISES = Object.freeze(
       inputId: "input-win-loss",
       functionCall: lastRollWithWinLoss
     },
+    {
+      title: "Most Rolled",
+      instruction: "Enter your guess:",
+      inputId: "input-most-rolled",
+      functionCall: mostRolled
+    },
+    {
+      title: "Guessing",
+      instruction: "Enter your guess:",
+      inputId: "input-guessing",
+      functionCall: guessing
+    },
+    {
+      title: "Advanced Guessing",
+      instruction: "Enter your guess:",
+      inputId: "input-advanced-guessing",
+      functionCall: advancedGuessing
+    },
   ]
 );
 
 class AppState {
   #rolls = [];
   #winCount = 0;
+  #points = 0;
+  #currentSwitcherIndex = -1;
 
   constructor() {
   }
 
-  addRoll(roll, addWinCount = false) {
+  addRoll(roll, addWinCount = false, points = 1) {
     this.#rolls.push(roll);
     if (addWinCount) {
       this.#winCount++;
+      this.#points += points;
     }
   }
 
@@ -39,6 +60,49 @@ class AppState {
 
   getWinRate() {
     return this.#winCount / this.#rolls.length;
+  }
+
+  getWinPoints() {
+    return this.#points;
+  }
+
+  getMostRolled() {
+    const counts = {
+      '0': 0,
+      '1': 0,
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0
+    };
+    for (const num of this.#rolls) {
+      counts[num - 1] += 1;
+    }
+    // console.log(counts);
+    // console.log(this.#rolls);
+    console.log(Object.entries(counts));
+    let mostFrequentRollIndex = 0;
+    for (const [key, value] of Object.entries(counts)) {
+      if (value > counts[mostFrequentRollIndex]) {
+        mostFrequentRollIndex = key;
+      }
+    }
+
+    return Number(mostFrequentRollIndex) + 1;
+  }
+
+  resetState() {
+    this.#rolls = [];
+    this.#winCount = 0;
+    this.#points = 0;
+  }
+
+  getLastSwitcherIndex() {
+    return this.#currentSwitcherIndex;
+  }
+
+  setLastSwitcherIndex(index) {
+    this.#currentSwitcherIndex = index;
   }
 }
 
@@ -64,6 +128,11 @@ const button = document.querySelector("#submit-button");
 button.addEventListener("click", function () {
   const switcher = UIkit.switcher("#switcher");
   const activeSwitcherIndex = switcher.index();
+
+  if (App.getLastSwitcherIndex() != activeSwitcherIndex) {
+    App.resetState();
+    App.setLastSwitcherIndex(activeSwitcherIndex);
+  }
 
   // console.log(`selected index is ${activeSwitcherIndex}`);
 
@@ -114,6 +183,87 @@ function lastRollWithWinLoss() {
   App.addRoll(randomDiceNumber, randomDiceNumber == diceGuess);
 
   result += "<br>" + `Your win rate is ${(App.getWinRate() * 100).toFixed(2)}%!`;
+
+  displayResults(result);
+}
+
+function mostRolled() {
+  const randomDiceNumber = rollDice();
+  const diceGuess = parseInt(document.querySelector("#input-most-rolled").value);
+
+  let result = "";
+
+  result += `You just rolled a ${randomDiceNumber}. You guessed ${diceGuess}.` + "<br>";
+
+  if (randomDiceNumber == diceGuess) {
+    result += "You win!";
+  } else {
+    result += "You lose!";
+  }
+
+  App.addRoll(randomDiceNumber);
+
+  result += "<br>" + `The number which you rolled the most is ${App.getMostRolled()}.`;
+
+  displayResults(result);
+}
+
+function guessing() {
+  const randomDiceNumber = rollDice();
+  const diceGuess = parseInt(document.querySelector("#input-guessing").value);
+
+  let result = "";
+  let pointsWon = 0;
+
+  result += `You just rolled a ${randomDiceNumber}. You guessed ${diceGuess}.` + "<br>";
+
+  if ([randomDiceNumber - 1, randomDiceNumber, randomDiceNumber + 1].includes(diceGuess)) {
+    if (randomDiceNumber == diceGuess) {
+      result += "You win 2 points!";
+      pointsWon = 2;
+    } else {
+      result += "You win 1 point!";
+      pointsWon = 1;
+    }
+  } else {
+    result += "You didn't win anything!";
+  }
+
+  // console.log(pointsWon);
+
+  App.addRoll(randomDiceNumber, pointsWon > 0, pointsWon);
+
+  result += "<br>" + `Your current score is ${App.getWinPoints()}!`;
+
+  displayResults(result);
+}
+
+function advancedGuessing() {
+  const randomDiceNumber = rollDice();
+  const diceGuess = parseInt(document.querySelector("#input-advanced-guessing").value);
+
+  let result = "";
+  let pointsWon = 0;
+
+  result += `You just rolled a ${randomDiceNumber}. You guessed ${diceGuess}.` + "<br>";
+
+  if ((diceGuess >= randomDiceNumber - 4) && (diceGuess <= randomDiceNumber + 4)) {
+    if (randomDiceNumber == diceGuess) {
+      result += "You win 5 points!";
+      pointsWon = 5;
+    } else {
+      pointsWon = 5 - Math.abs(diceGuess - randomDiceNumber);
+      result += `You win ${pointsWon} point(s)!`;
+    }
+  } else {
+    result += "You didn't win anything!";
+  }
+
+  // console.log(pointsWon);
+
+  App.addRoll(randomDiceNumber, pointsWon > 0, pointsWon);
+
+  result += "<br>" + `Your current score is ${App.getWinPoints()}!`;
 
   displayResults(result);
 }
